72fd43eb88146f85bda394cc70f6b215d6b8c67f
Rani Sidabutar <ranisidabutar58@gmail.com>
2026-09-24 03:55:18 +0700

    Initial commit

---
+++ src/main/java/com/pbi/tk/LatihanKlientServer.java
@@ -0,0 +1,15 @@
+
+
+package com.pbi.tk;
+
+
+public class LatihanKlientServer {
+
+    public static void main(String[] args) {
+         System.out.println("Nama  : Rani Demita Ekkliscia Sidabutar");
+        System.out.println("NIM   : 25110136");
+        System.out.println("Kelas : TK Grup C");
+        System.out.println("==============================");
+        System.out.println("Aplikasi klien siap dijalankan");
+    }
+}
